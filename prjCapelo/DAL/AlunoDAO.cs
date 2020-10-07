f9782330f2dd8cc424159c5a694566da340d0c8a
Lucas Pereira <lucaspgo@outlook.com>
2020-10-06 22:38:28 -0300

    Corrigir Erros

--- prjCapelo/DAL/AlunoDAO.cs
+++ prjCapelo/DAL/AlunoDAO.cs
@@ -33,7 +33,7 @@ namespace prjCapelo.DAL
             _context.SaveChanges();
         }
         public static List<Aluno> Listar() =>
-            _context.Aluno.Include(x => x.Pessoa).ToList();
+            _context.Aluno.ToList();
         public static Aluno BuscarPorId(int id) =>
             _context.Aluno.Find(id);
     }
